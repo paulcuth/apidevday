12ae3648088d73cbdc7eac7f361eca1d50c54c8c
Maarten Jacobs <maartenjacobs@hotmail.com>
2011-10-01 15:00:44 +0100

    Foursquare start support

---
+++ js/foursquare.js
@@ -0,0 +1,8 @@
+window.FOURSQ = (function($){
+  
+  var url = 'https://api.foursquare.com/v2/venues/search?ll=%coords',
+  client_id = 'H12WLMJCO5TBFVG1H2M4UNJZF1KL0ZHHHE5DGOJQDX1OGU3U',
+  secret = 'ZQC3AADBRTG5TFVUNQK2NKJAFAYI42IWPXJ4YRDQVCQ1MF5X';
+  
+  
+})($);
